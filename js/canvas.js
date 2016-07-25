7281453f9fdf51a0906f9212ed211c460de564e0
arii2016 <yoshiya2010@gmail.com>
2016-07-25 18:38:02 +0900

    undo機能実装

--- js/canvas.js
+++ js/canvas.js
@@ -7,12 +7,21 @@ var FILE_TYPE = {
     _SIZEOF : 3,
 };
 
-function Canvas(strCanvas)
+// -------------------------------------------------------------
+function Canvas(strCanvas, width, height)
 {
     var fabCanvas = new fabric.Canvas(strCanvas, {
+        width: width, 
+        height: height,
         imageSmoothingEnabled : false,      // 画像を平滑化しない
     });
-    var copyObjs = [];     // コピーするオブジェクト
+
+    // コピー・ペースト
+    var copyObjs = [];      // コピーするオブジェクト
+
+    // undo・redo
+    var stateArr = [fabCanvas.toJSON()];      // 変更ステータス保存配列
+    var evnDiseble = false;
 
     // -------------------------------------------------------------
     // オブジェクトを追加
@@ -20,6 +29,20 @@ function Canvas(strCanvas)
 
         fabric.loadSVGFromString(svgString, function(objects, options) {
             var obj = fabric.util.groupSVGElements(objects, options);
+
+            // JSON出力の追加項目を設定する
+            obj.toObject = (function(toObject) {
+                return function() {
+                    return fabric.util.object.extend(toObject.call(this), {
+                        hasRotatingPoint: this.hasRotatingPoint,
+                        lockRotation: this.lockRotation,
+                        lockScalingFlip: this.lockScalingFlip,
+                        lockUniScaling: this.lockUniScaling,
+                    });
+                };
+            })(obj.toObject);
+
+            // 画像は変形を制限する
             if (fileType == FILE_TYPE.IMG) {
                 // 画像の場合は、回転できないようにする
                 obj.hasRotatingPoint = false;
@@ -29,17 +52,49 @@ function Canvas(strCanvas)
                 // 比率をかけて拡大・縮小できないようにする
                 obj.lockUniScaling = true;
             }
+
+            // 追加
             fabCanvas.add(obj).renderAll();
         });
     };
     // -------------------------------------------------------------
-    // 元に戻す
     this.undo = function() {
-    }
+        if (1 < stateArr.length) {
+            evnDiseble = true;
+            fabCanvas.clear().renderAll();
+            fabCanvas.loadFromJSON(stateArr[stateArr.length - 2], function() {
+                fabCanvas.renderAll();
+                evnDiseble = false;
+            });
+            stateArr.pop();
+        }
+    };
     // -------------------------------------------------------------
-    // 取り消す
-    this.redo = function() {
-    }
+    // 変更ステータス更新
+    var updateState = function() {
+        if (evnDiseble) {
+            return;
+        }
+        if (stateArr.length > 4) {
+            stateArr.shift();
+        }
+        stateArr.push(fabCanvas.toJSON());
+    };
+    // -------------------------------------------------------------
+    // オブジェクト追加イベント
+    fabCanvas.on('object:added', function(options) {
+        updateState();
+    });
+    // -------------------------------------------------------------
+    // オブジェクト移動後イベント
+    fabCanvas.on('object:modified', function(options) {
+        updateState();
+    });
+    // -------------------------------------------------------------
+    // オブジェクト削除イベント
+    fabCanvas.on('object:removed', function(options) {
+        updateState();
+    });
     // -------------------------------------------------------------
     // コピー
     this.copyObj = function() {
@@ -66,7 +121,7 @@ function Canvas(strCanvas)
                 copyObjs.push(cloneObj);
             }
         }
-    }
+    };
     // -------------------------------------------------------------
     // 貼り付け
     this.pasteObj = function() {
@@ -80,7 +135,7 @@ function Canvas(strCanvas)
         fabCanvas.discardActiveObject();
         fabCanvas.discardActiveGroup();
         fabCanvas.renderAll();
-    }
+    };
     // -------------------------------------------------------------
     // 選択されているオブジェクトを削除
     this.deleteOneObj = function() {
@@ -99,68 +154,47 @@ function Canvas(strCanvas)
             fabCanvas.discardActiveGroup();
             fabCanvas.renderAll();
         }
-    }
+    };
     // -------------------------------------------------------------
     // すべてのオブジェクトを削除
     this.deleteAllObj = function() {
         fabCanvas.clear().renderAll();
-    }
-    // -------------------------------------------------------------
-    // canvas拡大
-    this.zoomIn = function(width, height) {
-    }
-    // -------------------------------------------------------------
-    // canvas縮小
-    this.zoomOut = function(width, height) {
-    }
-    // -------------------------------------------------------------
-    // canvasのサイズを変更
-    this.resizeCanvas = function(width, height) {
-    }
+    };
     // -------------------------------------------------------------
     // canvasの内容をSVG文字列に出力
     this.getSvg = function() {
         return fabCanvas.toSVG();
     };
     // -------------------------------------------------------------
+    // canvas拡大
+    this.zoomIn = function(width, height) {
+    };
+    // -------------------------------------------------------------
+    // canvas縮小
+    this.zoomOut = function(width, height) {
+    };
+    // -------------------------------------------------------------
     // 設定を保存
     this.save = function() {
+        return fabCanvas.toJSON();
     };
     // -------------------------------------------------------------
     // 設定を読み込み
-    this.load = function() {
+    this.load = function(jsonString) {
+        fabCanvas.loadFromJSON(jsonString, function(objects, options) {
+            fabCanvas.renderAll();
+        });
     };
     // -------------------------------------------------------------
 
-//// オブジェクト選択イベント
-//fabCanvas.on('object:selected', function(options) {
-//    //alert(options.target.fill);
-//});
-//// -------------------------------------------------------------
-//// オブジェクト選択解除イベント
-//fabCanvas.on('selection:cleared', function(options) {
-//    //alert('cleared!');
-//});
-//// -------------------------------------------------------------
 
 
-// 一つだけ選択されていた場合に取得可能(null)
-//var obj = fabCanvas.getActiveObject();
-
-// 複数選択されていた場合に取得可能(null)
-//var group = fabCanvas.getActiveGroup();
 
 // canvas上のすべてのオブジェクトを取得
 //var allObj = fabCanvas.getObjects(); 
 
 
-//canvas.item(0); // 先に追加したfabric.Rect(最初のオブジェクト)を参照します
-//canvas.remove(rect); // 先に追加したfabric.Rectを削除します
-
-// クローンを作成
-//canvas.item(0).clone();
-
-
 
 
 }
+// -------------------------------------------------------------
